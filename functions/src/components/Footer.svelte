7a65ddf50626ff60fb48c67c25c2a95912102139
David Barrow <Cheez@marks-mbp.home>
2020-10-31 21:36:58 +0000

    add footer to layout

---
+++ functions/src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let segment;
+</script>
+
+<style>
+    .footer {
+        display: flex;
+        background-color: rgba(0, 0, 0, 0.2);
+        position: fixed;
+        bottom: 0;
+    }
+
+    .footer p {
+        color:black;
+        margin: 0 1.5em;
+    }
+</style>
+
+<footer class="footer">
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, obcaecati provident, dolorum at exercitationem repellendus magnam fugiat doloremque maiores iusto labore. Accusantium mollitia possimus harum consectetur explicabo eaque iste delectus.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, obcaecati provident, dolorum at exercitationem repellendus magnam fugiat doloremque maiores iusto labore. Accusantium mollitia possimus harum consectetur explicabo eaque iste delectus.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, obcaecati provident, dolorum at exercitationem repellendus magnam fugiat doloremque maiores iusto labore. Accusantium mollitia possimus harum consectetur explicabo eaque iste delectus.</p>
+</footer>
